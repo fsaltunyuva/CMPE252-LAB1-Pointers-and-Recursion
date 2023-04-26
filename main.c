e720c37632c80cf0d4adf6e57567073e75d61190
Furkan Safa Altunyuva <furkansafaaltunyuva@gmail.com>
2023-04-26 11:20:54 +0300

    Correction in scanf() function

--- main.c
+++ main.c
@@ -8,14 +8,14 @@ double distance_between_two_points(double x1, double x2, double y1, double y2){
 void isRectangle(int *result, double *centerX, double *centerY)
 {
     double x1,x2,x3,x4,y1,y2,y3,y4;
-    scanf("%lf", x1);
-    scanf("%lf", x2);
-    scanf("%lf", x3);
-    scanf("%lf", x4);
-    scanf("%lf", y1);
-    scanf("%lf", y2);
-    scanf("%lf", y3);
-    scanf("%lf", y4);
+    scanf("%lf", &x1);
+    scanf("%lf", &y1);
+    scanf("%lf", &x2);
+    scanf("%lf", &y2);
+    scanf("%lf", &x3);
+    scanf("%lf", &y3);
+    scanf("%lf", &x4);
+    scanf("%lf", &y4);
 
     double diagonal_1 = distance_between_two_points(x3,x1,y3,y1);
     double diagonal_2 = distance_between_two_points(x4, x2, y4, y2);
